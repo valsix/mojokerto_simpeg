154a4c18e43bb2929a6aaf208e642b38b795d25d
Septian Eko <123433177+SepEko@users.noreply.github.com>
2024-04-04 19:12:11 +0700

    fix cuti

--- application/models/base/cuti.php
+++ application/models/base/cuti.php
@@ -10,6 +10,73 @@ class Cuti extends Entity{
 		$this->Entity(); 
 	}
 
+
+	function insert()
+	{
+		/*Auto-generate primary key(s) by next max value (integer) */
+		$this->setField("CUTI_ID", $this->getNextId("CUTI_ID","CUTI")); 
+
+		$str = "INSERT INTO CUTI (
+				   CUTI_ID, PEGAWAI_ID, JENIS_CUTI, 
+				   NO_SURAT, TANGGAL_PERMOHONAN, TANGGAL_SURAT, 
+				   LAMA, TANGGAL_MULAI, TANGGAL_SELESAI, 
+				   KETERANGAN, LAST_CREATE_USER, LAST_CREATE_DATE, LAST_CREATE_SATKER) 
+				VALUES (
+				  ".$this->getField("CUTI_ID").",
+				  '".$this->getField("PEGAWAI_ID")."',
+				  '".$this->getField("JENIS_CUTI")."',
+				  '".$this->getField("NO_SURAT")."',
+				  ".$this->getField("TANGGAL_PERMOHONAN").",
+				  ".$this->getField("TANGGAL_SURAT").",
+				  '".$this->getField("LAMA")."',
+				  ".$this->getField("TANGGAL_MULAI").",
+				  ".$this->getField("TANGGAL_SELESAI").",
+				  '".$this->getField("KETERANGAN")."',
+				  '".$this->getField("LAST_CREATE_USER")."',
+				  ".$this->getField("LAST_CREATE_DATE").",
+				  '".$this->getField("LAST_CREATE_SATKER")."'
+				)"; 
+				
+		$this->query = $str;
+		return $this->execQuery($str);
+    }
+
+    function update()
+	{
+		/*Auto-generate primary key(s) by next max value (integer) */
+		$str = "
+				UPDATE CUTI
+				SET    
+					   PEGAWAI_ID       = '".$this->getField("PEGAWAI_ID")."',
+					   JENIS_CUTI    = '".$this->getField("JENIS_CUTI")."',
+					   NO_SURAT             = '".$this->getField("NO_SURAT")."',
+					   TANGGAL_PERMOHONAN     = ".$this->getField("TANGGAL_PERMOHONAN").",
+					   TANGGAL_SURAT    = ".$this->getField("TANGGAL_SURAT").",
+					   LAMA    = '".$this->getField("LAMA")."',
+					   TANGGAL_MULAI  = ".$this->getField("TANGGAL_MULAI").",
+					   TANGGAL_SELESAI = ".$this->getField("TANGGAL_SELESAI").",
+					   KETERANGAN        = '".$this->getField("KETERANGAN")."',
+					  LAST_UPDATE_USER	= '".$this->getField("LAST_UPDATE_USER")."',
+					  LAST_UPDATE_DATE	= ".$this->getField("LAST_UPDATE_DATE").",
+					  LAST_UPDATE_SATKER	= '".$this->getField("LAST_UPDATE_SATKER")."'	
+				WHERE  CUTI_ID          = '".$this->getField("CUTI_ID")."'
+				"; 
+				$this->query = $str;
+		return $this->execQuery($str);
+    }
+
+
+    function delete()
+	{
+        $str = "DELETE FROM CUTI
+                WHERE 
+                  CUTI_ID = '".$this->getField("CUTI_ID")."'"; 
+				  
+		$this->query = $str;
+        return $this->execQuery($str);
+    }
+
+
 	function selectByParams($paramsArray=array(),$limit=-1,$from=-1, $statement='')
 	{
 		$str = "SELECT CUTI_ID, PEGAWAI_ID, JENIS_CUTI, 
--- application/views/main/cuti_add.php
+++ application/views/main/cuti_add.php
@@ -1,7 +1,7 @@
 <?
 include_once("functions/personal.func.php");
 
-$this->load->model("base/PengalamanKerja");
+$this->load->model("base/Cuti");
 
 $userpegawaimode= $this->userpegawaimode;
 $adminuserid= $this->adminuserid;
@@ -14,19 +14,35 @@ else
 $reqId= $this->input->get('reqId');
 $reqRowId= $this->input->get('reqRowId');
 
-$pengalaman= new PengalamanKerja();
-$pengalaman->selectByParams(array('PENGALAMAN_ID'=>$reqRowId));
-$pengalaman->firstRow();
-$reqPengalamanId = $pengalaman->getField('PENGALAMAN_ID');
-$reqInstansi 			= $pengalaman->getField('NAMA');
-$reqJabatan = $pengalaman->getField('JABATAN');
-$reqTglMulaiKerja = dateToPageCheck($pengalaman->getField('TANGGAL_KERJA'));
-$reqMasaKerjaTh = $pengalaman->getField('MASA_KERJA_TAHUN');
-$reqMasaKerjaBl = $pengalaman->getField('MASA_KERJA_BULAN');
-// echo $reqTmtJabatan;exit;
-$reqMode="update";
-// $reqMode="insert";
-$readonly = "readonly";
+if(empty($reqRowId))
+{
+	$reqMode="insert";
+}
+else
+{
+
+	$cuti = new Cuti();
+	$cuti->selectByParams(array('CUTI_ID'=>$reqRowId));
+	// echo $cuti->query;exit;
+	$cuti->firstRow();
+	$reqRowId					= (int)$cuti->getField('CUTI_ID');
+	$reqNoSurat 			= $cuti->getField('NO_SURAT');
+	$reqJenisCuti 		= $cuti->getField('JENIS_CUTI');
+	$reqTanggalPermohonan 		= dateToPageCheck($cuti->getField('TANGGAL_PERMOHONAN'));
+	$reqTanggalSurat 		= dateToPageCheck($cuti->getField('TANGGAL_SURAT'));
+	$reqTanggalMulai 		= dateToPageCheck($cuti->getField('TANGGAL_MULAI'));
+	$reqTanggalSelesai 		= dateToPageCheck($cuti->getField('TANGGAL_SELESAI'));
+	$reqLama		= $cuti->getField('LAMA');
+	$reqKeterangan		= $cuti->getField('KETERANGAN');
+
+	// echo $reqTmtJabatan;exit;
+	$reqMode="update";
+
+}
+
+	
+
+
 ?>
 
 <!-- Bootstrap core CSS -->
@@ -43,7 +59,7 @@ $readonly = "readonly";
 						<a class="" href="app/index/pegawai">Data Pegawai</a>
 					</li>
 					<li class="breadcrumb-item text-muted">
-						<a class="" href="app/index/pengalaman_kerja?reqId=<?=$reqId?>">Pengalaman Kerja</a>
+						<a class="" href="app/index/cuti?reqId=<?=$reqId?>">Cuti</a>
 					</li>
 					<li class="breadcrumb-item text-muted">
 						<a class="text-muted">Halaman Input</a>
@@ -70,49 +86,69 @@ $readonly = "readonly";
             <form class="form" id="ktloginform" method="POST" enctype="multipart/form-data">
 	        	<div class="card-body">
 	        		<div class="form-group row">
-	        			<label class="col-form-label text-right col-lg-2 col-sm-12">
-		        			Tanggal Mulai Kerja
-		        		</label>
-	        			<div class="col-lg-10 col-sm-12">
-	        				<div class="input-group date">
-		        				<input type="text" autocomplete="off" class="form-control" id="kttanggallahir" name="reqTglMulaiKerja" value="<?=$reqTglMulaiKerja?>" />
-		        				<div class="input-group-append">
-		        					<span class="input-group-text">
-		        						<i class="la la-calendar"></i>
-		        					</span>
-		        				</div>
-		        			</div>
+	        			<label class="col-form-label text-right col-lg-2 col-sm-12">Jenis Cuti</label>
+	        			<div class="col-lg-2 col-sm-12">
+	        				<select <?=$disabled?> name="reqJenisCuti" class="form-control">
+	        					<option value="1" <? if($reqJenisCuti == 1) echo 'selected';?>>Cuti Tahunan</option>				
+	        					<option value="2" <? if($reqJenisCuti == 2) echo 'selected';?>>Cuti Besar</option>				
+	        					<option value="3" <? if($reqJenisCuti == 3) echo 'selected';?>>Cuti Sakit</option>				
+	        					<option value="4" <? if($reqJenisCuti == 4) echo 'selected';?>>Cuti Bersalin</option>				
+	        					<option value="5" <? if($reqJenisCuti == 5) echo 'selected';?>>CLTN</option>				
+	        					<option value="6" <? if($reqJenisCuti == 6) echo 'selected';?>>Perpanjangan CLTN</option>
+	        					<option value="7" <? if($reqJenisCuti == 7) echo 'selected';?>>Cuti Menikah</option>
+	        					<option value="10" <? if($reqJenisCuti == 10) echo 'selected';?>>Cuti Alasan Penting</option>
+	        				</select>
 	        			</div>
 	        		</div>
 	        		<div class="form-group row">
-	        			<label class="col-form-label text-right col-lg-2 col-sm-12">Instansi</label>
-	        			<div class="col-lg-10 col-sm-12">
-	        				<input type="text" class="form-control" name="reqInstansi" id="reqInstansi" value="<?=$reqInstansi?>" />
+	        			<label class="col-form-label text-right col-lg-2 col-sm-12">No Surat</label>
+	        			<div class="col-lg-6 col-sm-12">
+	        				<input type="text" style="width:250px" <?=$read?> class="form-control" name="reqNoSurat" value="<?=$reqNoSurat?>" title="No surat harus diisi" class="required" />
 	        			</div>
 	        		</div>
 	        		<div class="form-group row">
-	        			<label class="col-form-label text-right col-lg-2 col-sm-12">Jabatan</label>
-	        			<div class="col-lg-10 col-sm-12">
-	        				<input type="text" class="form-control" name="reqJabatan" id="reqJabatan" value="<?=$reqJabatan?>" />
+	        			<label class="col-form-label text-right col-lg-2 col-sm-12">Tanggal Surat</label>
+	        			<div class="col-lg-4 col-sm-12">
+	        				<input type="text" style="width:100px" <?=$read?> id="reqTanggalSurat" class="form-control" name="reqTanggalSurat" maxlength="10"  value="<?=$reqTanggalSurat?>" />
+	        			</div>
+	        			<label class="col-form-label text-right col-lg-1 col-sm-12">Tanggal Mulai</label>
+	        			<div class="col-lg-4 col-sm-12">
+	        				<input type="text" style="width:100px" <?=$read?> id="reqTanggalMulai" class="form-control" name="reqTanggalMulai" maxlength="10"  value="<?=$reqTanggalMulai?>" />
 	        			</div>
 	        		</div>
+					<div class="form-group row">
+	        			<label class="col-form-label text-right col-lg-2 col-sm-12">Tanggal Permohonan</label>
+	        			<div class="col-lg-4 col-sm-12">
+	        				<input type="text" style="width:100px" <?=$read?> id="reqTanggalPermohonan" class="form-control" name="reqTanggalPermohonan" maxlength="10"  value="<?=$reqTanggalPermohonan?>" />
+	        			</div>
+	        			<label class="col-form-label text-right col-lg-1 col-sm-12">Tanggal Selesai</label>
+	        			<div class="col-lg-4 col-sm-12">
+	        				<input type="text" style="width:100px" <?=$read?> id="reqTanggalSelesai" class="form-control" name="reqTanggalSelesai" maxlength="10"  value="<?=$reqTanggalSelesai?>" />
+	        			</div>
+	        		</div>
+
 	        		<div class="form-group row">
-	        			<label class="col-form-label text-right col-lg-2 col-sm-12">Masa Kerja (Th)</label>
-	        			<div class="col-lg-10 col-sm-12">
-	        				<input type="text" class="form-control" name="reqMasaKerjaTh" id="reqMasaKerjaTh" value="<?=$reqMasaKerjaTh?>" />
+	        			<label class="col-form-label text-right col-lg-2 col-sm-12">Lama</label>
+	        			<div class="col-lg-6 col-sm-12">
+	        				<input type="text" style="width:250px" <?=$read?> class="form-control" name="reqLama" value="<?=$reqLama?>" />
 	        			</div>
 	        		</div>
+
+
 	        		<div class="form-group row">
-	        			<label class="col-form-label text-right col-lg-2 col-sm-12">Masa Kerja (Bln)</label>
-	        			<div class="col-lg-10 col-sm-12">
-	        				<input type="text" class="form-control" name="reqMasaKerjaBl" id="reqMasaKerjaBl" value="<?=$reqMasaKerjaBl?>" />
+	        			<label class="col-form-label text-right col-lg-2 col-sm-12">Keterangan</label>
+	        			<div class="col-lg-6 col-sm-12">
+	        				<textarea <?=$disabled?> class="form-control" name="reqKeterangan" cols="35"> <?=$reqKeterangan?></textarea>
 	        			</div>
 	        		</div>
+
+
 	        		<div class="card-footer">
 	        		<div class="row">
 	        			<div class="col-lg-9">
 	        				<input type="hidden" name="reqMode" value="<?=$reqMode?>">
-	        				<input type="hidden" name="reqTempValidasiId" value="<?=$reqTempValidasiId?>">
+	        				<input type="hidden" name="reqId" value="<?=$reqId?>">
+	        				<input type="hidden" name="reqRowId" value="<?=$reqRowId?>">
 	        				<button type="submit" id="ktloginformsubmitbutton" class="btn btn-light-success"><i class="fa fa-save" aria-hidden="true"></i> Simpan</button>
 	        			</div>
 	        		</div>
@@ -130,10 +166,29 @@ $readonly = "readonly";
 		// $('[data-toggle="tooltip"]').tooltip()
 	})
 
+	$("#reqTanggalSelesai, #reqTanggalMulai, #reqTanggalMulai, #reqTanggalSurat,#reqLama").keypress(function(e) {
+		if( e.which!=8 && e.which!=0 && (e.which<48 || e.which>57))
+		{
+			return false;
+		}
+	});
+
+
+	arrows= {leftArrow: '<i class="la la-angle-left"></i>', rightArrow: '<i class="la la-angle-right"></i>'};
+	$('#reqTanggalSurat,#reqTanggalMulai,#reqTanggalPermohonan,#reqTanggalSelesai').datepicker({
+		todayHighlight: true
+		, autoclose: true
+		, orientation: "bottom left"
+		, clearBtn: true
+		, format: 'dd-mm-yyyy'
+		, templates: arrows
+	});
+
+
 	var _buttonSpinnerClasses = 'spinner spinner-right spinner-white pr-15';
 	jQuery(document).ready(function() {
 		var form = KTUtil.getById('ktloginform');
-		var formSubmitUrl = "json-data/info_data_json/indentitaspegawai";
+		var formSubmitUrl = "json-main/cuti_json/add";
 		var formSubmitButton = KTUtil.getById('ktloginformsubmitbutton');
 		if (!form) {
 			return;
@@ -182,17 +237,30 @@ $readonly = "readonly";
 					success: function (response) {
 			        	// console.log(response); return false;
 			        	// Swal.fire("Good job!", "You clicked the button!", "success");
-			        	Swal.fire({
-			        		text: response.message,
-			        		icon: "success",
-			        		buttonsStyling: false,
-			        		confirmButtonText: "Ok",
-			        		customClass: {
-			        			confirmButton: "btn font-weight-bold btn-light-primary"
-			        		}
-			        	}).then(function() {
-			        		document.location.href = "app/index/pegawai_data";
-			        	});
+			        	data= response.message;
+			        	data= data.split("-");
+			        	rowid= data[0];
+			        	infodata= data[1];
+
+			        	if(rowid == "xxx")
+                        {
+                            Swal.fire("Error", infodata, "error");
+                        }
+                        else
+                        {
+                            Swal.fire({
+                                text: infodata,
+                                icon: "success",
+                                buttonsStyling: false,
+                                confirmButtonText: "Ok",
+                                customClass: {
+                                    confirmButton: "btn font-weight-bold btn-light-primary"
+                                }
+                            }).then(function() {
+                                document.location.href = "app/index/cuti?reqId=<?=$reqId?>";
+                                // window.location.reload();
+                            });
+                        }
 			        },
 			        error: function(xhr, status, error) {
 			        	var err = JSON.parse(xhr.responseText);
@@ -218,14 +286,5 @@ $readonly = "readonly";
 		});
 	});
 
-	arrows= {leftArrow: '<i class="la la-angle-left"></i>', rightArrow: '<i class="la la-angle-right"></i>'};
-	$('#kttanggallahir').datepicker({
-		todayHighlight: true
-		, autoclose: true
-		, orientation: "bottom left"
-		, clearBtn: true
-		, format: 'dd-mm-yyyy'
-		, templates: arrows
-	});
 
 </script>
