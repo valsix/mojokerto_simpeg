bfa4423b94c93d8b040b986c0857c80f9e529fed
anggorocn <anggorocahyon.if@gmail.com>
2024-05-06 12:22:25 +0700

    login

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -28,9 +28,15 @@ class login extends CI_Controller
 		$CI =& get_instance();
 		$configdata= $CI->config;
         $configvlxsessfolder= $configdata->config["vlxsessfolder"];
+		$genertecaptcha= $this->genertecaptcha();
+
+		$this->session->set_userdata('sessvgenertecaptcha', $genertecaptcha);
+		$this->sessvgenertecaptcha= $this->session->userdata("sessvgenertecaptcha");
+		// $_SESSION["sessvgenertecaptcha"]= $genertecaptcha;
 
 		$this->session->set_userdata('sessinfopesan'.$configvlxsessfolder, "");
 		$data['pesan']="";
+        // echo $genertecaptcha;exit;
 
 		$pg= "login";
 		$data = array(
@@ -42,7 +48,10 @@ class login extends CI_Controller
 
 	function action()
 	{
-		$this->load->library("crfs_protect"); $csrf = new crfs_protect('_crfs_login');
+		$this->load->library("crfs_protect"); 
+		$csrf = new crfs_protect('_crfs_login');
+		// print_r($csrf);exit;
+		
 		if (!$csrf->isTokenValid($_POST['_crfs_login']))
 		{
 		?>
@@ -61,7 +70,15 @@ class login extends CI_Controller
 		$reqUser= $this->input->post("reqUser");
 		$reqPasswd= $this->input->post("reqPasswd");
 		$reqCaptcha= $this->input->post("reqCaptcha");
+		$reqNewCaptcha= $this->input->post("reqNewCaptcha");
+		// echo $_SESSION['security_code'];exit;
 		
+		if($reqCaptcha != $reqNewCaptcha)
+    	{
+    		$this->session->set_userdata('sessinfopesan'.$configvlxsessfolder, "Captcha tidak sesuai.");
+			redirect ('login');
+    	}
+
 		if(!empty($reqUser) AND !empty($reqPasswd))
 		{
 			$respon = $this->kauth->cekuserloginpersonal($reqUser,$reqPasswd);
@@ -96,5 +113,17 @@ class login extends CI_Controller
 		$this->kauth->unsetcekuserloginpersonal();
 		// $this->session->sess_destroy();
 		redirect ('login');
+	}
+
+	function genertecaptcha(){
+		$color= substr(uniqid(),-2);
+		$vreturn= strtoupper(substr(md5($color), 0,5));
+		return $vreturn;
+	}
+
+	function captcha(){
+		$genertecaptcha= $this->genertecaptcha();
+		$this->session->set_userdata("sessvgenertecaptcha", $genertecaptcha);
+		echo $genertecaptcha;
 	}
 }
