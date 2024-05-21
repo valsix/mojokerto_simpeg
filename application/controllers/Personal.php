e1838bf0d1d386f81c00b18256ef0ec96efa415b
ronedha86 <ronedha86@gmail.com>
2024-05-21 16:25:30 +0700

    masih simple dan query, belum simpan

---
+++ application/controllers/Personal.php
@@ -0,0 +1,101 @@
+<?php
+// ini_set('display_errors', '1');
+// ini_set('display_startup_errors', '1');
+// error_reporting(E_ALL);
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+include_once("functions/image.func.php");
+include_once("functions/string.func.php");
+
+class Personal extends CI_Controller {
+	
+	function __construct() {
+		parent::__construct();
+		//kauth
+
+		$CI =& get_instance();
+		$configdata= $CI->config;
+        $configvlxsessfolder= $configdata->config["vlxsessfolder"];
+
+        $redirectlogin= "";
+        if(!empty($this->session->userdata("adminuserid".$configvlxsessfolder)))
+        {
+        	$redirectlogin= $this->session->userdata("adminuserid".$configvlxsessfolder);
+        }
+
+        if(empty($redirectlogin))
+		{
+			redirect('login');
+		}
+
+		$this->adminuserid= $this->session->userdata("adminuserid".$configvlxsessfolder);
+		$this->adminuserloginnama= $this->session->userdata("adminuserloginnama".$configvlxsessfolder);
+		$this->adminusernama= $this->session->userdata("adminusernama".$configvlxsessfolder);
+		$this->adminusergroupid= $this->session->userdata("adminusergroupid".$configvlxsessfolder);
+		$this->adminuserpegawaiid= $this->session->userdata("adminuserpegawaiid".$configvlxsessfolder);
+		$this->adminsatkerid= $this->session->userdata("adminsatkerid".$configvlxsessfolder);
+		$this->sesstree= $this->session->userdata("sesstree".$configvlxsessfolder);
+		$this->sessdatatree= $this->session->userdata("sessdatatree".$configvlxsessfolder);
+		$this->adminuserPegawaiProses= $this->session->userdata("adminuserPegawaiProses".$configvlxsessfolder);
+		$this->adminuserDUKProses= $this->session->userdata("adminuserDUKProses".$configvlxsessfolder);
+		$this->adminuserKGBProses= $this->session->userdata("adminuserKGBProses".$configvlxsessfolder);
+		$this->adminuserKPProses= $this->session->userdata("adminuserKPProses".$configvlxsessfolder);
+		$this->adminuserPensiunProses= $this->session->userdata("adminuserPensiunProses".$configvlxsessfolder);
+		$this->adminuserAnjabProses= $this->session->userdata("adminuserAnjabProses".$configvlxsessfolder);
+		$this->adminuserMutasiProses= $this->session->userdata("adminuserMutasiProses".$configvlxsessfolder);
+		$this->adminuserHukumanProses= $this->session->userdata("adminuserHukumanProses".$configvlxsessfolder);
+		$this->adminuserMasterProses= $this->session->userdata("adminuserMasterProses".$configvlxsessfolder);
+		$this->adminuserLihatProses= $this->session->userdata("adminuserLihatProses".$configvlxsessfolder);
+		$this->adminuserBidangPembinaan= $this->session->userdata("adminuserBidangPembinaan".$configvlxsessfolder);
+		$this->adminuserBidangDokumentasi= $this->session->userdata("adminuserBidangDokumentasi".$configvlxsessfolder);
+		$this->adminuserBidangPendidikan= $this->session->userdata("adminuserBidangPendidikan".$configvlxsessfolder);
+		$this->adminuserBidangMutasi= $this->session->userdata("adminuserBidangMutasi".$configvlxsessfolder);
+	}
+	
+	public function index()
+	{
+		$pg = $this->uri->segment(3, "home");
+
+		$reqId = $this->input->get("reqId");
+		$adminuserid= $this->adminuserid;
+		// echo $adminuserid;exit;
+
+		$view = array(
+			'pg' => $pg,
+			'linkBack' => $file."_detil"
+		);
+
+		$data = array(
+			'breadcrumb' => $breadcrumb,
+			'content' => $this->load->view("personal/".$pg,$view,TRUE),
+			'pg' => $pg
+		);
+		// print_r($view);exit;
+		
+		$this->load->view('personal/index', $data);
+	}
+
+	public function loadUrl()
+	{		
+		$reqFolder = $this->uri->segment(3, "");
+		$reqFilename = $this->uri->segment(4, "");
+		$reqParse1 = $this->uri->segment(5, "");
+		$reqParse2 = $this->uri->segment(6, "");
+		$reqParse3 = $this->uri->segment(7, "");
+		$reqParse4 = $this->uri->segment(8, "");
+		$reqParse5 = $this->uri->segment(9, "");
+		$data = array(
+			'reqParse1' => urldecode($reqParse1),
+			'reqParse2' => urldecode($reqParse2),
+			'reqParse3' => urldecode($reqParse3),
+			'reqParse4' => urldecode($reqParse4),
+			'reqParse5' => urldecode($reqParse5)
+		);
+
+		if($reqFolder == "personal" || $reqFolder == "public" || $reqFolder == "admin")
+			$this->session->set_userdata('currentUrl', $reqFilename);
+		
+		$this->load->view($reqFolder.'/'.$reqFilename, $data);
+	}
+	
+}
