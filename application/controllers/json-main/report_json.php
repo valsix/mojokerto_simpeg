46b92b2260cc8909973ec1445e4ca7e93aa5436d
Septian Eko <123433177+SepEko@users.noreply.github.com>
2024-05-27 17:07:29 +0700

    monitoring rekap golongan

---
+++ application/controllers/json-main/report_json.php
@@ -0,0 +1,169 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+include_once("functions/string.func.php");
+include_once("functions/date.func.php");
+include_once("functions/class-list-util.php");
+include_once("functions/class-list-util-serverside.php");
+
+class report_json extends CI_Controller {
+
+	function __construct() {
+		parent::__construct();
+		//kauth
+
+		$CI =& get_instance();
+		$configdata= $CI->config;
+        $configvlxsessfolder= $configdata->config["vlxsessfolder"];
+
+        $redirectlogin= "";
+        if(!empty($this->session->userdata("adminuserid".$configvlxsessfolder)))
+        {
+        	$redirectlogin= $this->session->userdata("adminuserid".$configvlxsessfolder);
+        }
+
+        if(empty($redirectlogin))
+		{
+			redirect('login');
+		}
+
+		$this->adminuserid= $this->session->userdata("adminuserid".$configvlxsessfolder);
+		$this->adminsatkerid= $this->session->userdata("adminsatkerid".$configvlxsessfolder);
+		$this->sess_satkerid= $this->adminsatkerid;
+		$this->adminuserloginnama= $this->session->userdata("adminuserloginnama".$configvlxsessfolder);
+		$this->adminusernama= $this->session->userdata("adminusernama".$configvlxsessfolder);
+		$this->adminusergroupid= $this->session->userdata("adminusergroupid".$configvlxsessfolder);
+		$this->adminuserpegawaiid= $this->session->userdata("adminuserpegawaiid".$configvlxsessfolder);
+	}
+
+	function json_rekap_golongan()
+	{
+		$this->load->model("base/Rekap");
+
+		$set= new Rekap();
+
+		if ( isset( $_REQUEST['columnsDef'] ) && is_array( $_REQUEST['columnsDef'] ) ) {
+			$columnsDefault = [];
+			foreach ( $_REQUEST['columnsDef'] as $field ) {
+				$columnsDefault[ $field ] = "true";
+			}
+		}
+		// print_r($columnsDefault);exit;
+
+		$displaystart= -1;
+		$displaylength= -1;
+		$no=1;
+
+		$arrinfodata= [];
+
+		$statement= "";
+		// $sOrder = " ORDER BY A.TANGGAL ASC";
+		$sOrder = " ";
+		$set->selectByParamsBkppRekapGolongan(array(), $displaylength, $displaystart, $statement, $sOrder);
+		// echo $set->query;exit;
+		while ($set->nextRow()) 
+		{
+			$row= [];
+			foreach($columnsDefault as $valkey => $valitem) 
+			{
+				if ($valkey == "SORDERDEFAULT")
+					$row[$valkey]= "1";
+				else if ($valkey == "TANGGAL")
+				{
+					$row[$valkey]= dateToPageCheck($set->getField($valkey));
+				}
+				else if ($valkey == "NO")
+				{
+					$row[$valkey]= $no;
+				}
+				else if ($valkey == "GAJI"  )
+				{
+					$row[$valkey]= currencyToPage($set->getField(trim($valkey)));
+				}
+				else
+					$row[$valkey]= $set->getField($valkey);
+			}
+			$no++;
+			array_push($arrinfodata, $row);
+		}
+
+		// get all raw data
+		$alldata = $arrinfodata;
+		// print_r($alldata);exit;
+
+		$data = [];
+		// internal use; filter selected columns only from raw data
+		foreach ( $alldata as $d ) {
+			// $data[] = filterArray( $d, $columnsDefault );
+			$data[] = $d;
+		}
+
+		// count data
+		$totalRecords = $totalDisplay = count( $data );
+
+		// filter by general search keyword
+		if ( isset( $_REQUEST['search'] ) ) {
+			$data         = filterKeyword( $data, $_REQUEST['search'] );
+			$totalDisplay = count( $data );
+		}
+
+		if ( isset( $_REQUEST['columns'] ) && is_array( $_REQUEST['columns'] ) ) {
+			foreach ( $_REQUEST['columns'] as $column ) {
+				if ( isset( $column['search'] ) ) {
+					$data         = filterKeyword( $data, $column['search'], $column['data'] );
+					$totalDisplay = count( $data );
+				}
+			}
+		}
+
+		// sort
+		if ( isset( $_REQUEST['order'][0]['column'] ) && $_REQUEST['order'][0]['dir'] ) {
+			$column = $_REQUEST['order'][0]['column'];
+			if(count($columnsDefault) - 2 == $column){}
+			else
+			{
+				$dir    = $_REQUEST['order'][0]['dir'];
+				usort( $data, function ( $a, $b ) use ( $column, $dir ) {
+					$a = array_slice( $a, $column, 1 );
+					$b = array_slice( $b, $column, 1 );
+					$a = array_pop( $a );
+					$b = array_pop( $b );
+
+					if ( $dir === 'asc' ) {
+						return $a > $b ? true : false;
+					}
+
+					return $a < $b ? true : false;
+				} );
+			}
+		}
+
+		// pagination length
+		if ( isset( $_REQUEST['length'] ) ) {
+			$data = array_splice( $data, $_REQUEST['start'], $_REQUEST['length'] );
+		}
+
+		// return array values only without the keys
+		if ( isset( $_REQUEST['array_values'] ) && $_REQUEST['array_values'] ) {
+			$tmp  = $data;
+			$data = [];
+			foreach ( $tmp as $d ) {
+				$data[] = array_values( $d );
+			}
+		}
+
+		$result = [
+		    'recordsTotal'    => $totalRecords,
+		    'recordsFiltered' => $totalDisplay,
+		    'data'            => $data,
+		];
+
+		header('Content-Type: application/json');
+		echo json_encode( $result, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);	
+	}
+
+	
+
+	
+}
+?>
