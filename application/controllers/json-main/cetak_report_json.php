1cae67f7871cee58de57465499b5ca74d7347444
Septian Eko <123433177+SepEko@users.noreply.github.com>
2024-05-30 18:02:22 +0700

    rekap pensiun all (masih proses)

--- application/controllers/json-main/cetak_report_json.php
+++ application/controllers/json-main/cetak_report_json.php
@@ -1196,6 +1196,247 @@ class cetak_report_json extends CI_Controller {
 		unlink($down);
 	}
 
+	function pensiun()
+	{
+		/* INCLUDE FILE */
+		require 'lib/PHPExcel.php';
+		include_once("functions/date.func.php");
+		include_once("functions/string.func.php");
+		include_once("functions/default.func.php");
+		//set_time_limit(3);
+		ini_set("memory_limit","500M");
+		ini_set('max_execution_time', 520);
+
+		$reqId= $this->input->get('reqId');
+		$reqFilter= $this->input->get('reqFilter');
+		$reqPeriode= $this->input->get('reqPeriode');
+		$reqTahun= $this->input->get('reqTahun');
+
+		if($reqPeriode == "1") 
+		{
+			$infoPeriode= 'Semester I (1 Januari - 30 Juni)';
+		}
+		elseif($reqPeriode == 2)
+		{
+			$infoPeriode= 'Semester II (1 Juli - 30 Desember)';
+		}
+		else
+		{
+			$infoPeriode= 'Semua Periode';
+		}
+
+		$tgl=date('Y-m-d');
+
+
+		$objPHPexcel= PHPExcel_IOFactory::load('Templates/report/pensiun.xlsx');
+		$style = array(
+			'borders' => array(
+				'allborders' => array(
+
+					'style' => PHPExcel_Style_Border::BORDER_THIN
+				)				
+			),
+			'alignment' => array(
+				'horizontal' => PHPExcel_Style_Alignment::HORIZONTAL_LEFT,
+			),
+			'font'  => array(
+				'size' => 8,
+				'name'  => 'Tahoma'
+
+			),
+		);
+
+		$styleT = array(
+			'borders' => array(
+				'allborders' => array(
+
+					'style' => PHPExcel_Style_Border::BORDER_THIN
+				)				
+			),
+			'alignment' => array(
+				'horizontal' => PHPExcel_Style_Alignment::HORIZONTAL_CENTER,
+			),
+			'font'  => array(
+				'size' => 8,
+				'name'  => 'Tahoma'
+
+			),
+		);
+
+		$styleTengah = array(
+			'alignment' => array(
+				'horizontal' => PHPExcel_Style_Alignment::HORIZONTAL_CENTER,
+			),
+			'font'  => array(
+				'size' => 8,
+				'name'  => 'Tahoma'
+
+			),
+		);
+
+
+		$styleTengahC = array(
+			'borders' => array(
+				'allborders' => array(
+
+					'style' => PHPExcel_Style_Border::BORDER_THIN
+				)				
+			),
+			'alignment' => array(
+				'horizontal' => PHPExcel_Style_Alignment::HORIZONTAL_CENTER,
+			),
+			'font'  => array(
+				'size' => 8,
+				'name'  => 'Tahoma'
+
+			),
+			'fill' => array(
+				'type' => PHPExcel_Style_Fill::FILL_SOLID,
+				'color' => array('rgb' => '9BC2E6')
+			),
+		);
+
+
+
+
+
+		$objWorksheet = $objPHPexcel->getActiveSheet();
+
+		$field= array("NO","NAMA","TOT_11","TOT_12","TOT_13", "TOT_14", "TOT_GOL1", 
+		"TOT_21","TOT_22","TOT_23", "TOT_24", "TOT_GOL2",
+		"TOT_31","TOT_32","TOT_33", "TOT_34", "TOT_GOL3",
+		"TOT_41","TOT_42","TOT_43", "TOT_44", "TOT_GOL4");
+
+
+		if($reqFilter == ""){
+
+		}
+
+		$pesan= "KEADAAN ".strtoupper($infoPeriode)." TAHUN ".$reqTahun;
+
+		$objWorksheet->setCellValue("C4",$pesan);
+
+		$statement = "  ";
+
+		$this->load->model("base/Rekap");
+		$set = new Rekap();
+		$set->selectByParamsReportPensiunAll(array(), -1, -1, $statement, $reqPeriode, $reqTahun);	
+		// $urut=1;
+		$nomor=1;
+		$kolomawal=1;
+		$row = 12;
+		// echo $set->query; exit; 
+		while($set->nextRow()){	
+
+			$index_kolom= 0;
+
+			for($i=0; $i<count($field); $i++)
+			{
+			
+				$kolom= toAlpha($index_kolom);	$kolom= toAlpha($index_kolom);
+				// print_r($kolom);
+				$objWorksheet->getStyle($kolom.$row)->applyFromArray($style);
+				if($field[$i] == "NO")
+				{
+					$objWorksheet->setCellValueExplicit($kolom.$row,$nomor, PHPExcel_Cell_DataType::TYPE_STRING);
+					$objWorksheet->getStyle("A".$row)->applyFromArray($styleT);
+				}
+				else if($field[$i] == "NAMA" )
+				{
+					$objWorksheet->setCellValue($kolom.$row,$set->getField($field[$i]));
+				}
+				else if($field[$i] == "TOTAL_NEW" )
+				{
+					$objWorksheet->setCellValue($kolom.$row,$set->getField($field[$i])+$set->getField("TOT_STAF"));
+					$objWorksheet->getStyle($kolom.$row)->applyFromArray($styleT);
+				}
+				else
+				{
+					$objWorksheet->setCellValue($kolom.$row,$set->getField($field[$i]));
+					$objWorksheet->getStyle($kolom.$row)->applyFromArray($styleT);
+				}
+
+				$index_kolom++;
+			}
+
+
+			$nomor++;
+			$row++;
+		} 
+		// print_r($index_kolom);exit;
+
+		$objWorksheet->mergeCells('A'.$row.':'.'B'.$row);
+		$objWorksheet->setCellValue("A".$row,"JUMLAH DIPINDAHKAN");
+		$objWorksheet->getStyle("A".$row)->getFont()->setBold( true );
+		$objWorksheet->getStyle("A".$row)->applyFromArray($styleTengahC);
+		
+		for($i=2; $i<=$index_kolom-1; $i++)
+		{
+			// print_r($kolom);
+			$kolom= toAlpha($i);
+			$rowAwal=setToAlpha($i, 11); $rowAkhir=setToAlpha($i, $row-1);
+			// print_r($kolom.$row-1."/"."=SUM(".$rowAwal.":".$rowAkhir.")");
+			// $objWorksheet->setCellValue($kolom.$row,"=SUM(".$rowAwal.":".$rowAkhir.")");
+			$objWorksheet->getStyle($kolom.$row)->applyFromArray($styleTengahC);
+		}
+
+		$rowtotal=$row+1;
+
+		$objWorksheet->mergeCells('A'.$rowtotal.':'.'B'.$rowtotal);
+		$objWorksheet->setCellValue("A".$rowtotal,"JUMLAH TOTAL");
+		$objWorksheet->getStyle("A".$rowtotal)->getFont()->setBold( true );
+		$objWorksheet->getStyle("A".$rowtotal)->applyFromArray($styleTengahC);
+		
+		for($i=2; $i<=$index_kolom-1; $i++)
+		{
+			// print_r($kolom);
+			$kolom= toAlpha($i);
+			$rowAwal=setToAlpha($i, 11); $rowAkhir=setToAlpha($i, $rowtotal-1);
+			// print_r($kolom.$row-1."/"."=SUM(".$rowAwal.":".$rowAkhir.")");
+			$objWorksheet->setCellValue($kolom.$rowtotal,"=SUM(".$rowAwal.":".$rowAkhir.")");
+			$objWorksheet->getStyle($kolom.$rowtotal)->applyFromArray($styleTengahC);
+		}
+
+		$rowttd=$rowtotal+2;
+
+		// print_r($rowttd);exit;
+		$objWorksheet->setCellValue("AD".$rowttd,"KEPALA BADAN KEPEGAWAIAN DAN");
+		$objWorksheet->getStyle("AD".$rowttd)->getFont()->setBold( true );
+		$objWorksheet->getStyle("AD".$rowttd)->applyFromArray($styleTengah);
+		$rowttd=$rowtotal+3;
+		$objWorksheet->setCellValue("AD".$rowttd,"PENGEMBANGAN SUMBER DAYA MANUSIA");
+		$objWorksheet->getStyle("AD".$rowttd)->getFont()->setBold( true );
+		$objWorksheet->getStyle("AD".$rowttd)->applyFromArray($styleTengah);
+		$rowttd=$rowtotal+4;
+		$objWorksheet->setCellValue("AD".$rowttd,"KABUPATEN MOJOKERTO");
+		$objWorksheet->getStyle("AD".$rowttd)->getFont()->setBold( true );
+		$objWorksheet->getStyle("AD".$rowttd)->applyFromArray($styleTengah);
+
+
+		$rowttd=$rowtotal+9;
+		$objWorksheet->setCellValue("AC".$rowttd,"NIP :");
+		$objWorksheet->getStyle("AC".$rowttd)->getFont()->setBold( true );
+		$objWorksheet->getStyle("AC".$rowttd)->applyFromArray($styleTengah);
+
+
+		// exit;
+
+		$objWriter = PHPExcel_IOFactory::createWriter($objPHPexcel, 'Excel5');
+		$objWriter->save('Templates/report/laporan_skpd_pensiun_excel.xls');
+
+		$down= 'Templates/report/laporan_skpd_pensiun_excel.xls';
+		header('Content-Description: File Transfer');
+		header('Content-Type: application/vnd.ms-excel');
+		header('Content-Disposition: attachment; filename='.basename($down));
+		header('Content-Transfer-Encoding: binary');
+		header('Expires: 0');
+		header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+		header('Pragma: public');
+		header('Content-Length: ' . filesize($down));
+		readfile($down);
+		unlink($down);
+	}
+
 
 	
 
---
+++ application/views/main/laporan_skpd_pensiun_all.php
@@ -0,0 +1,308 @@
+<?php
+$reqId= $this->input->get('reqId');
+
+
+$pgtitle= $pg;
+$pgtitle= churuf(str_replace("_", " ", str_replace("master_", "", $pgtitle)));
+
+
+
+
+$arrtabledatath1= array(
+     array("label"=>"Unit Kerja", "field"=> "NAMA", "display"=>"",  "width"=>"", "rowspan"=>"2", "colspan"=>"")
+    , array("label"=>"Gol Ruang", "field"=> "", "display"=>"",  "width"=>"", "rowspan"=>"", "colspan"=>"4")
+    , array("label"=>"Jenis Kelamin", "field"=> "", "display"=>"",  "width"=>"", "rowspan"=>"", "colspan"=>"2")
+    , array("label"=>"Status Kawin", "field"=> "", "display"=>"",  "width"=>"", "rowspan"=>"", "colspan"=>"4")
+);
+
+
+$arrtabledatath2= array(
+     array("label"=>"I", "field"=> "TOT_GOL1", "display"=>"",  "width"=>"", "rowspan"=>"", "colspan"=>"")
+    , array("label"=>"II", "field"=> "TOT_GOL2", "display"=>"",  "width"=>"", "rowspan"=>"", "colspan"=>"")
+    , array("label"=>"III", "field"=> "TOT_GOL3", "display"=>"",  "width"=>"", "rowspan"=>"", "colspan"=>"")
+    , array("label"=>"IV", "field"=> "TOT_GOL4", "display"=>"",  "width"=>"", "rowspan"=>"", "colspan"=>"")
+    , array("label"=>"L", "field"=> "TOT_L", "display"=>"",  "width"=>"", "rowspan"=>"", "colspan"=>"")
+    , array("label"=>"P", "field"=> "TOT_P", "display"=>"",  "width"=>"", "rowspan"=>"", "colspan"=>"")
+    , array("label"=>"KW", "field"=> "TOT_KW", "display"=>"",  "width"=>"", "rowspan"=>"", "colspan"=>"")
+    , array("label"=>"BL", "field"=> "TOT_BL", "display"=>"",  "width"=>"", "rowspan"=>"", "colspan"=>"")
+    , array("label"=>"JD", "field"=> "TOT_JD", "display"=>"",  "width"=>"", "rowspan"=>"", "colspan"=>"")
+    , array("label"=>"DD", "field"=> "TOT_DD", "display"=>"",  "width"=>"", "rowspan"=>"", "colspan"=>"")
+    
+
+);
+
+
+$arrtabledata= array(
+      array("label"=>"UNIT KERJA", "field"=> "NAMA", "display"=>"",  "width"=>"", "rowspan"=>"3", "colspan"=>"")
+    , array("label"=>"I", "field"=> "TOT_GOL1", "display"=>"",  "width"=>"", "rowspan"=>"", "colspan"=>"")
+    , array("label"=>"II", "field"=> "TOT_GOL2", "display"=>"",  "width"=>"", "rowspan"=>"", "colspan"=>"")
+    , array("label"=>"III", "field"=> "TOT_GOL3", "display"=>"",  "width"=>"", "rowspan"=>"", "colspan"=>"")
+    , array("label"=>"IV", "field"=> "TOT_GOL4", "display"=>"",  "width"=>"", "rowspan"=>"", "colspan"=>"")
+    , array("label"=>"L", "field"=> "TOT_L", "display"=>"",  "width"=>"", "rowspan"=>"", "colspan"=>"")
+    , array("label"=>"P", "field"=> "TOT_P", "display"=>"",  "width"=>"", "rowspan"=>"", "colspan"=>"")
+    , array("label"=>"KW", "field"=> "TOT_KW", "display"=>"",  "width"=>"", "rowspan"=>"", "colspan"=>"")
+    , array("label"=>"BL", "field"=> "TOT_BL", "display"=>"",  "width"=>"", "rowspan"=>"", "colspan"=>"")
+    , array("label"=>"JD", "field"=> "TOT_JD", "display"=>"",  "width"=>"", "rowspan"=>"", "colspan"=>"")
+    , array("label"=>"DD", "field"=> "TOT_DD", "display"=>"",  "width"=>"", "rowspan"=>"", "colspan"=>"")
+   
+);
+
+
+
+$arrTahun= setTahunLoop(1,2);
+
+
+
+?>
+
+<!-- SELECT2 -->
+<!-- <link href="lib/select2/select2.min.css" rel="stylesheet"> -->
+<link href="lib/select2totreemaster/src/select2totree.css" rel="stylesheet">
+<script src="lib/select2/select2.min.js"></script>
+<script src="lib/select2totreemaster/src/select2totree.js"></script>
+
+<div class="subheader py-2 py-lg-6 subheader-solid" id="kt_subheader">
+    <div class="container-fluid d-flex align-items-center justify-content-between flex-wrap flex-sm-nowrap">
+        <div class="d-flex align-items-center flex-wrap mr-1">
+            <div class="d-flex align-items-baseline flex-wrap mr-5">
+                <ul class="breadcrumb breadcrumb-transparent breadcrumb-dot font-weight-bold p-0 my-2 font-size-sm">
+                    <li class="breadcrumb-item text-muted">
+                        <a class="text-muted">Menu Report</a>
+                    </li>
+                    <li class="breadcrumb-item text-muted">
+                        <a class="text-muted"><?=$pgtitle?></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+<div class="d-flex flex-column-fluid">
+    <div class="container">
+
+        <div class="card card-custom">
+            <div class="card-header">
+                <div class="card-title">
+                    <span class="card-icon">
+                        <i class="flaticon2-notepad text-primary"></i>
+                    </span>
+                    <h3 class="card-label"><?=$pgtitle?></h3>
+                </div>
+                <div class="card-toolbar">
+                    <!--begin::Dropdown-->
+                    <div class="dropdown dropdown-inline mr-2">
+                    	<button class="btn btn-light-success" id="btnCetak"><i class="fa fa-file-excel-o" aria-hidden="true"></i> Cetak</button>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="col-md-12">
+                <div class="area-filter">
+                    <div class="row mb-8">
+                        <div class="col-md-6" style="margin-top: 10px">
+                            <label>Periode:</label>
+                            <select name="reqPeriode" id="reqPeriode" class="form-control">
+                               <!-- <option value="0">Semua</option> -->
+                               <option value="1">Semester I (1 Januari - 30 Juni)</option>
+                               <option value="2">Semester II (1 Juli - 30 Desember)</option>          
+                          </select>
+                        </div>
+                        <div class="col-md-6" style="margin-top: 10px">
+                            <label>Tahun:</label>
+                            <select name="reqTahun" id="reqTahun" class="form-control">
+                              <?
+                              for($i=0;$i<count($arrTahun);$i++)
+                              {
+                                  ?>
+                                  <option value="<?=$arrTahun[$i]?>" <? if($reqTahun == $arrTahun[$i]) { ?> selected <? } ?>><?=$arrTahun[$i]?></option>
+                                  <?      
+                              }
+                              ?>    
+                          </select>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card-body">
+                <table class="table table-bordered table-hover table-checkable" id="kt_datatable" style="margin-top: 13px !important;width: 100%">
+                    <thead>
+                        <tr>
+                            <?php
+                            foreach($arrtabledatath1 as $valkey => $valitem) 
+                            {
+                                $infotablelabel= $valitem["label"];
+                                $infotablecolspan= $valitem["colspan"];
+                                $infotablerowspan= $valitem["rowspan"];
+                                $infowidth= $valitem["width"];
+
+                                if(!empty($infotablecolspan))
+                                {
+                                }
+
+                                if(!empty($infotablelabel))
+                                {
+                                    ?>
+                                    <th style="text-align:center; width: <?=$infowidth?>%" colspan='<?=$infotablecolspan?>' rowspan='<?=$infotablerowspan?>'><?=$infotablelabel?></th>
+                                    <?
+                                }
+                            }
+                            ?>
+                        </tr>
+
+                        <tr>
+                            <?php
+                            foreach($arrtabledatath2 as $valkeyn => $valitemn) 
+                            {
+                                $infotablelabel= $valitemn["label"];
+                                $infotablecolspan= $valitemn["colspan"];
+                                $infotablerowspan= $valitemn["rowspan"];
+                                $infowidth= $valitemn["width"];
+
+                                if(!empty($infotablecolspan))
+                                {
+                                }
+
+                                if(!empty($infotablelabel))
+                                {
+                                    ?>
+                                    <th style="text-align:center; width: <?=$infowidth?>%" colspan='<?=$infotablecolspan?>' rowspan='<?=$infotablerowspan?>'><?=$infotablelabel?></th>
+                                    <?
+                                }
+                            }
+                            ?>
+                        </tr>
+
+                    </thead>
+                </table>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+
+<a href="#" id="triggercari" style="display:none" title="triggercari">triggercari</a>
+<script type="text/javascript">
+var datanewtable;
+var infotableid= "kt_datatable";
+var carijenis= "";
+var arrdata= <?php echo json_encode($arrtabledata); ?>;
+// console.log(arrdata);
+var indexfieldid= arrdata.length - 1;
+var indexvalidasiid= arrdata.length - 3;
+var indexvalidasihapusid= arrdata.length - 4;
+var valinfoid = '';
+var valinfovalidasiid = '';
+var valinfovalidasihapusid = '';
+
+
+jQuery(document).ready(function() {
+    var jsonurl= "json-main/report_json/json_rekap_pensiun?reqPeriode="+ $("#reqPeriode").val()+"&reqTahun="+ $("#reqTahun").val();
+    ajaxserverselectsingle.init(infotableid, jsonurl, arrdata);
+
+    var infoid= [];
+    $('#'+infotableid+' tbody').on( 'click', 'tr', function () {
+        // untuk pilih satu data, kalau untuk multi comman saja
+        $('#'+infotableid+' tbody tr').removeClass('selected');
+
+        var el= $(this);
+        el.addClass('selected');
+
+        var dataselected= datanewtable.DataTable().row(this).data();
+        // console.log(dataselected);
+        // console.log(Object.keys(dataselected).length);
+        fieldinfoid= arrdata[indexfieldid]["field"]
+        fieldvalidasiid= arrdata[indexvalidasiid]["field"]
+        fieldvalidasihapusid= arrdata[indexvalidasihapusid]["field"]
+        valinfoid= dataselected[fieldinfoid];
+        valinfovalidasiid= dataselected[fieldvalidasiid];
+        valinfovalidasihapusid= dataselected[fieldvalidasihapusid];
+        if (valinfovalidasiid == null)
+        {
+            valinfovalidasiid = '';
+        }
+    });
+
+    $('#'+infotableid+' tbody').on( 'dblclick', 'tr', function () {
+      $("#btnUbahData").click();    
+    });
+
+    $("#reqPeriode").change(function() { 
+        jsonurl= "json-main/report_json/json_rekap_pensiun?reqSatkerId=<?=$reqId?>&reqPeriode=" + $("#reqPeriode").val()+"&reqTahun=" + $("#reqTahun").val();
+            datanewtable.DataTable().ajax.url(jsonurl).load();
+   });
+
+    $("#reqTahun").change(function() { 
+        jsonurl= "json-main/report_json/json_rekap_pensiun?reqSatkerId=<?=$reqId?>&reqPeriode=" + $("#reqPeriode").val()+"&reqTahun=" + $("#reqTahun").val();
+            datanewtable.DataTable().ajax.url(jsonurl).load();
+   });
+
+    $("#btnAdd, #btnUbahData").on("click", function () {
+        btnid= $(this).attr('id');
+
+        if(valinfoid == "" && btnid == "btnUbahData")
+        {
+            Swal.fire({
+                text: "Pilih salah satu data terlebih dahulu.",
+                icon: "error",
+                buttonsStyling: false,
+                confirmButtonText: "Ok",
+                customClass: {
+                    confirmButton: "btn font-weight-bold btn-light-primary"
+                }
+            });
+            return false;
+        }
+
+        if(btnid == "btnUbahData")
+            vpilihid= valinfoid;
+        else
+            vpilihid= "";
+
+        varurl= "app/index/master_pangkat_add?reqId="+vpilihid;
+        
+        document.location.href = varurl;
+    });
+
+    $("#buttoncaridetil").on("click", function () {
+        carijenis= "2";
+        calltriggercari();
+    });
+    $("#triggercari").on("click", function () {
+
+        if(carijenis == "1")
+        {
+            pencarian= $('#'+infotableid+'_filter input').val();
+            datanewtable.DataTable().search( pencarian ).draw();
+        }
+        else
+        {
+            
+        }
+    });
+
+    $("#btnCetak").on("click", function () {
+        reqPeriode= $("#reqPeriode").val();
+        reqTahun= $("#reqTahun").val();
+        varurl= "json-main/cetak_report_json/pensiun?reqPeriode="+reqPeriode+"&reqTahun="+reqTahun;
+        // window.open(varurl, 'window name', 'window settings');
+        window.open(varurl, '_blank');
+    }); 
+
+});
+
+function calltriggercari()
+{
+    $(document).ready( function () {
+      $("#triggercari").click();      
+    });
+}
+
+function submitForm() {
+   $("#ktloginformsubmitbutton").click(); 
+}
+</script>
